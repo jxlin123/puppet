2bd4bd4311cc5d8465d02d38bb1d3535fa990451
John Xinyu Lin <johnxinyu@ocf.berkeley.edu>
2019-09-04 20:32:39 -0700

    Remove deleted Puppet resources
    Since I've deleted some files (see my older commits), those files
    should no longer be specified in Puppet.
    
    I'm still leaving behind some of the ppd stuff cooperc wrote earlier,
    so they actually get removed from the desktops.

--- modules/ocf/manifests/packages/cups.pp
+++ modules/ocf/manifests/packages/cups.pp
@@ -15,12 +15,6 @@ class ocf::packages::cups {
     # set default paper size
     '/etc/papersize':
       content => 'letter';
-    # set printer configurations
-    '/etc/cups/printers.conf':
-      source  => 'puppet:///modules/ocf/packages/cups/printers.conf',
-      group   => 'lp',
-      require => Package['cups', 'cups-bsd'],
-      notify  => Service['cups'];
     '/etc/cups/ppd/single.ppd':
       # content => epp('ocf/cups/raster.ppd.epp', { 'double' => false }),
       # group   => 'lp',
@@ -32,19 +26,7 @@ class ocf::packages::cups {
       # group   => 'lp',
       ensure  => absent,
       require => Package['cups', 'cups-bsd'],
-      notify  => Service['cups'];
-    # set rasterizing filter
-    '/usr/lib/cups/filter/raster-filter':
-      source => 'puppet:///modules/ocf/packages/cups/raster-filter',
-      mode   => '0755';
-    # set tea4cups configurations
-    '/etc/cups/tea4cups.conf':
-      source  => 'puppet:///modules/ocf/packages/cups/tea4cups.conf',
-      require => Package['cups-tea4cups'];
-    # set convert_failure script for raster-filter
-    '/usr/local/bin/convert_failure':
-      source => 'puppet:///modules/ocf/packages/cups/convert_failure',
-      mode   => '0755';
+      notify  => Service['cups']
   }
 
   service { 'cups':
